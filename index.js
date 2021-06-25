af06c59399e34ea43911a52436a485c6303f22e5
Marc Pàmpols <mpampols@gmail.com>
2021-06-26 00:13:55 +0200

    Added build folder

--- index.js
+++ index.js
@@ -5,13 +5,18 @@ const { prependOnceListener } = require("process");
 const app = express();
 
 var morgan = require("morgan");
-morgan.token('data', function (req, res) { return JSON.stringify(req.body) })
+morgan.token("data", function (req, res) {
+  return JSON.stringify(req.body);
+});
 
 // middleware
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded());
-app.use(morgan(":method :url :status :res[content-length] - :response-time ms :data"));
+app.use(
+  morgan(":method :url :status :res[content-length] - :response-time ms :data")
+);
+app.use(express.static("build"));
 
 let persons = [
   {
