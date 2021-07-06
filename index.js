2edca932a38384ba1b2c17d795e5416c2eeb8476
Marc Pampols <mpampols@gmail.com>
2021-07-06 17:03:52 +0200

    Finished part 3c

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ morgan.token("data", function (req, res) {
   return JSON.stringify(req.body);
 });
 
-// middleware
+// Middlewares
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded());
@@ -24,22 +24,17 @@ app.use(
 );
 app.use(express.static("build"));
 
-let persons = [{
-    id: 1,
-    name: "John",
-    number: "111 111 111",
-  },
-  {
-    id: 2,
-    name: "Tony",
-    number: "222 222 222",
-  },
-  {
-    id: 3,
-    name: "Joseph",
-    number: "333 333 333",
-  },
-];
+// Middleware: Error handler
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } 
+
+  next(error)
+}
+app.use(errorHandler)
 
 app.get("/", (request, response) => {
   response.send("<h1>Hello World!</h1>");
@@ -60,46 +55,72 @@ app.get("/api/persons", (request, response) => {
 });
 
 app.get("/api/persons/:id", (request, response) => {
-  const id = Number(request.params.id);
-  const person = persons.find((person) => person.id === id);
-
-  if (person) {
-    response.json(person);
-  } else {
-    response.status(400).end();
-  }
+  Person.findById(request.params.id)
+    .then(person => {
+      if (person) {
+        response.json(person)
+      } else {
+        response.status(404).end()
+      }
+    })
+    .catch(error => next(error))
 });
 
-app.delete("/api/persons/:id", (request, response) => {
-  const id = Number(request.params.id);
-  persons = persons.filter((person) => person.id !== id);
+app.delete("/api/persons/:id", (request, response, next) => {
+  Person.findByIdAndRemove(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
 
   response.status(204).end();
 });
 
-app.post("/api/persons", (request, response) => {
-  const maxId = persons.length > 0 ? Math.max(...persons.map((p) => p.id)) : 0;
+app.put("/api/persons/:id", (request, response, next) => {
+  const body = request.body
+  const person = {
+    name: request.body.name,
+    number: request.body.number
+  }
 
+  Person.findByIdAndUpdate(request.params.id, person, {new: true})
+    .then(updatedPerson => {
+      response.json(updatedPerson)
+    })
+    .catch(error => next(error))
+});
+
+app.post("/api/persons", (request, response, next) => {
   if (!request.body.name || !request.body.number) {
     return response.status(400).json({
       error: "Name or number are missing.",
     });
   }
 
-  if (persons.find((person) => person.name === request.body.name)) {
-    return response.status(400).json({
-      error: "Person with the same name already exists. Name must be unique.",
-    });
-  }
-
   const person = new Person({
     name: request.body.name,
     number: request.body.number,
+    id: request.body.id
   });
 
-  person.save().then(savedPerson => {
-    response.json(savedPerson);
-  })
+  // If a person with the same name already exists. We choose to update the number.
+  Person.findById(request.params.id)
+    .then(foundPerson => {
+      if (foundPerson) {
+        // Name exists, updating...
+        Person.findByIdAndUpdate(request.params.id, foundPerson, {new: true})
+        .then(updatedPerson => {
+          response.json(updatedPerson)
+        })
+        .catch(error => next(error))
+      } else {
+        // Name is new, creating...
+        person.save().then(savedPerson => {
+          response.json(savedPerson)
+        })
+      }
+    })
+    .catch(error => next(error))
 });
 
 const PORT = process.env.PORT || 3001;
--- models/person.js
+++ models/person.js
@@ -1,23 +1,33 @@
 const mongoose = require('mongoose')
 
 const url = process.env.MONGODB_URI
-console.log('connecting to', url)
+console.log('Connecting to', url)
 
 mongoose.connect(url, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useFindAndModify: false,
-    useCreateIndex: true })
-    .then(result => {
-        console.log('connected to MongoDB')
-    })
-    .catch((error) => {
-        console.log('error connecting to MongoDB:', error.message)
-    })
+    useCreateIndex: true
+  })
+  .then(result => {
+    console.log('Connected to MongoDB')
+  })
+  .catch((error) => {
+    console.log('Error connecting to MongoDB:', error.message)
+  })
 
 const personSchema = new mongoose.Schema({
-    name: String,
-    number: String,
+  name: String,
+  number: String,
+  id: String
+});
+
+personSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
 });
 
 module.exports = mongoose.model('Person', personSchema)
